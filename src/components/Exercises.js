14757567f91a05fa5300c5d8c69ae32b323f5622
dhruvpahujaaa <dhruvpahujaaa@gmail.com>
2025-08-16 03:44:36 +0530

    Fix: Correct exerciseOptions import in Exercises.js component

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material';
 
-import { fetchData, exerciseOption } from '../utils/fetchData';
+import { fetchData, exerciseOptions } from '../utils/fetchData';
 
 import ExerciseCard from './ExerciseCard';
 import Loader from './Loader';
